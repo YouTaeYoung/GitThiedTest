5e714a4d97bde6170be0e42d92acd41b793f444a
dbxodud89 <dbxodud89@naver.com>
2021-10-15 11:11:38 +0900

    제발..

--- src/git/firstgiy.java
+++ src/git/firstgiy.java
@@ -6,10 +6,9 @@ public class firstgiy
 	{
 		System.out.println("세번째시도");
 		System.out.println("오잉 오잉 오잉");
-		System.out.println("회사ㅇ2에서 작업");
+		System.out.println("회사 에서 작업");
 		System.out.println("왜안대");
 		System.out.println("넘어려워");
-		System.out.println("개어렵누 git");
-		System.out.println("넘어려웡...");
+		
 	}
 }
