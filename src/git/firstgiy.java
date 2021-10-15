42dc9738d38dd9fc730784f498f862aa1a061301
dbxodud89 <dbxodud89@naver.com>
2021-10-15 11:14:55 +0900

    집에서 연습 1

--- src/git/firstgiy.java
+++ src/git/firstgiy.java
@@ -5,11 +5,9 @@ public class firstgiy
 	public static void main(String[] args)
 	{
 		System.out.println("세번째시도");
-		System.out.println("오잉 오잉 오잉");
-		System.out.println("회사 에서 작업");
-		System.out.println("왜안대");
-		System.out.println("넘어려워");
-		System.out.println("왜안되는거지");
+		System.out.println("집에서 시도");
+		
+		
 		
 	}
 }
