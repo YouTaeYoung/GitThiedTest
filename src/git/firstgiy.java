4f92ec275cf4e7df88f8fb4a137c389ab8cfbe05
dbxodud89 <dbxodud89@naver.com>
2021-10-15 11:06:42 +0900

    연습 다시 하기

--- src/git/firstgiy.java
+++ src/git/firstgiy.java
@@ -9,5 +9,7 @@ public class firstgiy
 		System.out.println("회사ㅇ2에서 작업");
 		System.out.println("왜안대");
 		System.out.println("넘어려워");
+		System.out.println("개어렵누 git");
+		System.out.println("넘어려웡...");
 	}
 }
