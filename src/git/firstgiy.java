6a8f67bcb588550bd6c99df60b761c7b1a2fd268
dbxodud89 <dbxodud89@naver.com>
2021-10-15 10:59:22 +0900

    집에서

--- src/git/firstgiy.java
+++ src/git/firstgiy.java
@@ -6,8 +6,9 @@ public class firstgiy
 	public static void main(String[] args)
 	{
 		System.out.println("세번째시도");
-		System.out.println("회사에서 작업");
+		System.out.println("회사ㅇ2에서 작업");
 		System.out.println("왜안대");
+		System.out.println("넘어려워");
 
 	}
 
